76b3bcb81e0c8374e71f43fc946f069491557486
chrisjyh <pureredgrace@gmail.com>
2022-12-29 19:23:07 +0900

    list_change

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const useful_site = {
+    'excalidraw' : 'https://excalidraw.com/',
+    'mycolor-space' : 'https://mycolor.space/',
+    'grabient' : 'https://www.grabient.com/',
+    'box-shadows' : 'https://shadows.brumm.af/',
+    'grid-generator' : 'https://cssgrid-generator.netlify.app/',
+    'keyframes' : 'https://keyframes.app/animate/',
+    'svg-backgrounds' : 'https://www.svgbackgrounds.com/',
+    'undraw' : 'https://undraw.co/',
+    'fontjoy' : 'https://fontjoy.com/',
+    'sizzy' : 'https://sizzy.co/'
+}

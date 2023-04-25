f887938e3fc1c5ad70b67436d821465a7326c2da
jooeunho <pureredgrace@gmail.com>
2023-04-25 15:15:01 +0900

    23.04.25 side_nav create

--- js/setting.js
+++ js/setting.js
@@ -1,1 +1,21 @@
-console.log("11111")
+window.onload = function () {
+    // 버튼 선택자
+    const btn = document.querySelectorAll('.btn');
+    const side_nav = document.getElementById('side_nav')
+    
+    // 버튼 클릭함수 버튼일때 전부 여기서 실행
+    btn.forEach((target) => target.addEventListener('click', function(){ 
+        let parent_this = this;
+        temp = parent_this.className.split(' ')
+        // 버거 클릭할때
+        if(temp.includes('burger')){
+            parent_this.classList.toggle('clicked');
+            side_nav.classList.toggle('show');
+        }
+
+
+     }));
+
+
+}
+
